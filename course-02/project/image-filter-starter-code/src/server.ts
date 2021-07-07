120deb0a3b7bcd5316c53dda48eed4b432ff58a8
eperuza <jeremy.riv@outlook.com>
2021-07-06 20:42:40 -0400

    first endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
+
 (async () => {
 
   // Init the Express application
@@ -28,7 +29,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+  app.get('/filteredimage', (req,res) => {
+    const URL = req.query.image_url;
+    if(!URL){
+      return res.status(400).send('An image URL is required.')
+    }
+    return filterImageFromURL(URL)
+    .then(img => {
+      return res.status(200).sendFile(img, () => {
+        deleteLocalFiles([img])
+      });
+    })
+    .catch(err => {
+      console.log(err)
+      return res.status(400).send('Error filtering at specified URL')
+    })
+  })
   //! END @TODO1
   
   // Root Endpoint
